177da7189f517171fbc7884995c05c60d33f8936
VictorManuelSalas <victorsalasnz@gmail.com>
2022-09-30 23:31:57 -0500

    avance del http y json

--- AppMenu/src/app/app.module.ts
+++ AppMenu/src/app/app.module.ts
@@ -7,10 +7,24 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+//Se importa el metdo necesario para el plugin HTTP
+import { HttpClientModule } from '@angular/common/http'
+
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    //Se mete aqui como import para poder trabajar en el
+    HttpClientModule
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, 
+      useClass: IonicRouteStrategy 
+    }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- AppMenu/src/app/customers/customers.page.ts
+++ AppMenu/src/app/customers/customers.page.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+
+//Importacion para que funcione aqui en HTTP
+import { HttpClient } from '@angular/common/http'
+//Importacion de la propiedad map para poder mapiar el json ya que con esto nos permite generar un nuevo arreglo mediante otro 
+import { map } from 'rxjs/operators'
+
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.page.html',
@@ -7,15 +13,39 @@ import { Router } from '@angular/router';
 })
 export class CustomersPage implements OnInit {
 
-  //Se le da el parametro de private router el cual se toma de Router el cual se importo arriba
-  constructor(private router: Router) { }
+  
 
+  //Se le da el parametro de private router el cual se toma de Router el cual se importo arriba
+  constructor(
+    private router: Router,
+    private http: HttpClient) { }
+
+    //Esta funcion es la que se ejecuta al refrescar o cargar la pagina
   ngOnInit() {
+    this.getUsers().subscribe(res => {
+      console.log("Res", res)
+      this.users = res;
+    });
   }
+
+  //Variable creada para almacenar todos los datos recibidos, esta variable es de tipo cualquiera y es una arreglo vacio
+  users: any = [];
 
   //Esta es la funcion que nos permite cambiar la pagina gracias a su contenido el cual usa la ruta de home
   goToHome(){
     this.router.navigate(['/home'])
   }
 
+  getUsers(){
+    //Aqui es que regrese la variable http que se definio en el contructor de arriba el cual obtendra con el get los valores de la sig liga
+    return this.http.get("assets/files/customers.json")
+    //Pipe
+    .pipe(
+      //Se usa map el cual declara una variable res el cual sera de tipo cualqiera
+      map(( res:any ) => {
+        //Aqui es que regrese la variable declarada en el map el cual recibe el valor data del json
+        return res.data;
+      })
+    )
+  }
 }
