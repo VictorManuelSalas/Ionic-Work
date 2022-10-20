0b1caa685d29550ab837ec251c6cd0c95ce73112
VictorManuelSalas <victorsalasnz@gmail.com>
2022-10-20 13:50:40 -0500

    como hacer compoenetes

--- AppMenu/src/app/home/home.module.ts
+++ AppMenu/src/app/home/home.module.ts
@@ -7,9 +7,12 @@ import { IonicModule } from '@ionic/angular';
 import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
+import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
   imports: [
+    //Este es el compoenente el cual se maneja como modulo 
+    ComponentsModule,
     CommonModule,
     FormsModule,
     IonicModule,
